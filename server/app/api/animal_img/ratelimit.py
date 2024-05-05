f409e2f9c37e9c871bcfea53faccf21090e4cac2
FreeFly0125 <simonbergman516@gmail.com>
2024-05-05 20:20:46 +0200

    feat: add rate limit for 1 req per minute

--- server/app/api/animal_img/ratelimit.py
+++ server/app/api/animal_img/ratelimit.py
@@ -1,5 +1,18 @@
+import time
+
+
 class RateLimiter:
-    pass
+    def __init__(self) -> None:
+        self.past_req_timestamp = 0
+
+    def check_request_rate(self) -> bool:
+        current_time = time.time()
+
+        if current_time - self.past_req_timestamp >= 60:
+            self.past_req_timestamp = current_time
+            return True
+        else:
+            return False
 
 
 rateLimiter = RateLimiter()
--- server/app/api/animal_img/routes.py
+++ server/app/api/animal_img/routes.py
@@ -7,6 +7,12 @@ router = APIRouter()
 
 @router.get(path="/fetchimgs", response_model=object, status_code=status.HTTP_200_OK)
 def img_fetch_request():
+    if rateLimiter.check_request_rate() is False:
+        raise HTTPException(
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+            detail={"message": "You are limited only 1 req per minute"},
+        )
+    
     (img_url, fetch_time, animal_type) = fetch_animal_imgs()
     return {
         "url": img_url,
