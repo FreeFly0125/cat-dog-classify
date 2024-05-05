bd24868fe014b7adb15e7e5525e8d0ac816ea2ed
FreeFly0125 <simonbergman516@gmail.com>
2024-05-05 19:06:15 +0200

    init commit

---
+++ server/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get(path="/health", status_code=200)
+def checkServerHealth():
+    return {"status": "Hello, World"}
