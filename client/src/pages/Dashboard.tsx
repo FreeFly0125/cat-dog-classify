b53b959e7775c94947b7e7e060d9536dc3157570
FreeFly0125 <simonbergman516@gmail.com>
2024-05-06 02:37:18 +0200

    feat: request image info per one minute

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -9,6 +9,25 @@ export const Dashboard: React.FC = withMainlayout(() => {
   const { animals, updateAnimals } =
     useContext(MainContext);
 
+  const fetchData = async () => {
+    const animalInfo = await axios.get<IAnimalInfoResponse>(
+      "http://localhost:8000/fetchimgs"
+    );
+    updateAnimals({
+      url: animalInfo.data.url,
+      type: animalInfo.data.type,
+      fetchtime: animalInfo.data.fetch_time,
+    });
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 60000);
+
+    return () => clearInterval(intervalId);
+ }, [animals]);
+
   return (
     <div className="flex w-full justify-center pt-24">
       <div className="grid grid-cols-5 justify-between gap-5 tablet:grid-cols-3 mobile:grid-cols-1">
