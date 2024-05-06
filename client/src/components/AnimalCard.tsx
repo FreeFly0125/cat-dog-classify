25af8ddc227b7367dd38e2371a34ac1552c7a306
FreeFly0125 <simonbergman516@gmail.com>
2024-05-06 03:01:00 +0200

    fix: display animal type as uppercase

--- client/src/components/AnimalCard.tsx
+++ client/src/components/AnimalCard.tsx
@@ -12,7 +12,7 @@ export const AnimalCard: React.FC<CardProps> = ({animal}) => {
     <div className="p-2 rounded-lg border-2">
       <img src={imageUrl} alt="Animal Image" className="w-48 border-2 rounded-lg" />
       <div className="flex justify-around">
-        <p>Dog</p>
+        <p>{animal.type.toUpperCase()}</p>
         <p>{animal.fetchtime.toFixed(2)}s</p>
       </div>
     </div>
